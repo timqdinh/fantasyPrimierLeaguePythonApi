b0879a5f6a38b0a89c61af670f23b970d263401a
Tim Dinh <timdinh@192-168-1-110.tpgi.com.au>
2020-06-26 19:01:33 +1000

    included requirements.txt, updated to python3

--- src/simple_fetch.py
+++ src/simple_fetch.py
@@ -1,16 +1,12 @@
 import json
-import urllib.request
-import urllib.parse
-from operator import itemgetter
+import urllib3
 
-playerDetailsUrl = 'https://fantasy.premierleague.com/api/bootstrap-static/'
-playerList = urllib.request.urlopen(playerDetailsUrl)
+http = urllib3.PoolManager()
+bootstrapStaticUrl = 'https://fantasy.premierleague.com/api/bootstrap-static/'
+playerList = http.request('GET', 'http://httpbin.org/robots.txt')
+playerList = urllib.request.urlopen(bootstrapStaticUrl)
 playersDetailsData = json.loads(playerList.read().decode('utf-8'))
 print(playersDetailsData)
-
-def fetch_player_postion(playersDetail, playerId):
-    return playerDetail
-
 
 events = playersDetailsData['events']
 game_settings = playersDetailsData['game_settings']
@@ -19,13 +15,6 @@ teams = playersDetailsData['teams']
 elements = playersDetailsData['elements']
 element_stats = playersDetailsData['element_stats']
 element_types = playersDetailsData['element_types']
-
-# for player in elements:
-#     if player['element_type'] == 4 and float(player['selected_by_percent']) > 15 and player['now_cost'] == 80 and player['news'] == '':
-#         for team in teams:
-#             if player['team_code'] == team['code']:
-#                 print(player['first_name'] + ' ' + player['second_name'], player['selected_by_percent'], player['now_cost'], team['name'])
-
 
 playersHistoryDetailsUrl = 'https://fantasy.premierleague.com/api/event/1/live/'
 playersHistoryDetails = urllib.request.urlopen(playersHistoryDetailsUrl)
